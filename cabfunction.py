955e95d53a6342f3c66913859ae3b1b2de7a6a76
mythili-123 <58969248+mythili-123@users.noreply.github.com>
2019-12-20 09:55:06 +0530

    Add files via upload

---
+++ cabfunction.py
@@ -0,0 +1,83 @@
+def my_function():
+ print("enter the register below:")
+ user=input("enter the username:")
+ pwd=int(input("confirm pwd:"))
+ correctpwd=int(input("pwd"))
+if pwd=correct:
+ print("LOGGEDIN")
+ source=0
+ destination=int(intput("enter the destination:"))
+ print("choose cab type:")
+ print(1,"mini")
+ print(2,"sedan")
+ print(3,"xuv")
+ print(4,"prime")
+ cabtype=int(intput("choose cabtype"))
+  a="mini"
+  b="sedan"
+  c="xuv"
+  d="prime"
+ if cabtype==a:
+    if (destination<=5):
+       print("charge=2)
+     elif(destination<5)and(destination<=10):
+       print("charge=4")
+     elif(destination<10)and(destination<=20):
+       print("charge=6")
+     elif(destination<20)and(destination<=35):
+       print("charge=8")
+     else:
+       print("enter the destination between 25km")
+     elif cabtype==b:
+   if (destination<=5):
+       print("charge=5")
+    elif(destination<5)and(destination<=10):
+       print("charge=5")
+    elif(destination<10)and(destination<=20):
+       print("charge=10")
+    elif(destination<20)and(destination<=25):
+       print("charge=15")
+    else:
+       print("enter the destination between 25km")  
+    elif cabtype==c:
+   if (destination<=5):
+      print("charge=6")
+    elif(destination<5)and(destination<=10):
+      print("charge=12")
+    elif(destinantion<10)and(destination<=20):
+      print("charge=18")
+    elif(destination<20)and(destination<=25):
+      print("charge=24") 
+    else:
+      print("enter the destination between 25km") 
+    elif cabtype==d:
+   if (destination<=5):
+     print("charge=7")
+   elif(destination<5)and(destination<=10):
+     print("charge=14")
+   elif(destinantion<10)and(destination<=20):
+     print("charge=21")
+   elif(destination<20)and(destination<=25):
+     print("charge=28")
+   else:
+     print("enter the destination between 25km")
+   else:
+     print("enter the correct cab type")
+     print("find your invoice below")
+     print("username",user)
+     print("no of km",destination)
+   else:
+     print("please enter the correct password")
+      str1="yes"
+      str2="no"
+      quit=(input("if you want to continue the trip,please enter yes or no")   
+      if(quit=="str1"):
+      my_function()
+   else:
+     print("Thank you")
+
+
+
+
+ 
+
